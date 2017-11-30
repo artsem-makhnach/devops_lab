8d3ba20e2314763e2f9f302a4451e5c666627c14
artsem-makhnach <artsem_makhnach@epam.com>
2017-11-30 16:06:24 +0300

    fix script

--- homework1/task1.sh
+++ homework1/task1.sh
@@ -1,23 +1,14 @@
 sudo yum install -y git gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel xz xz-devel
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-#echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-#echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
-#echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
-#exec $SHELL
+git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 source ~/.bash_profile
 
 pyenv install 2.7.14
-
-echo "Eee, Vasya!"
-
 pyenv install 3.5.4
 
-git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-
-source ~/.bashrc
 
 pyenv virtualenv 2.7.14 python2
 pyenv virtualenv 3.5.4 python3
