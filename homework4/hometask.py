e0b040026c1ac227e433c9596e229e2083e534e0
artsem-makhnach <artsem_makhnach@epam.com>
2017-12-05 17:25:25 +0300

    add docstrings

--- homework4/hometask.py
+++ homework4/hometask.py
@@ -1,3 +1,4 @@
+"""Monitoring"""
 import datetime
 import time
 import config
@@ -5,7 +6,9 @@ import psutil
 
 
 class Mem(object):
+    """Class for memory statement collection"""
     def collect_mem(self):
+        """Function for collecting memory state"""
         mem = psutil.virtual_memory()
         mem_usage = mem[2]
         mem_str = "Memory usage, %: " + str(mem_usage) + "; "
@@ -13,7 +16,9 @@ class Mem(object):
 
 
 class Cpu(object):
+    """Class for CPU statement collection"""
     def collect_cpu(self):
+        """Function for collecting CPU state"""
         cpu = psutil.cpu_percent(interval=1, percpu=True)
         cpu_str = ""
         for i in range(len(cpu)):
@@ -22,26 +27,32 @@ class Cpu(object):
 
 
 class Disk(object):
+    """Class for disk statement collection"""
     def collect_disk(self):
+        """Function for collecting disk state"""
         disk = psutil.disk_io_counters()
         disk_str = "Disk read, MBytes: "+str(disk[2]/(1024**2))+"; Disk write, MBytes: "+str(disk[3]/(1024**2))+"; "
         return disk_str
 
 
 class Net(object):
+    """Class for network statement collection"""
     def collect_net(self):
+        """Function for collecting network state"""
         net = psutil.net_io_counters()
         net_str = "Net packets sent: " + str(net[2]) + "; Net packets received: " + str(net[3]) + ";"
         return net_str
 
 
 class Monitoring(Mem, Cpu, Disk, Net):
-
+    """Main monitoring class"""
     def __init__(self, output_format, minutes):
+        """init method for main executable class"""
         self.output_format = output_format
         self.minutes = minutes
 
     def main(self):
+        """main executable method"""
         my_file = open("output." + self.output_format, 'w')
         iterator = 1
         try:
